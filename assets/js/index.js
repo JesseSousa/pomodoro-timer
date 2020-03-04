f966f64eaab466e799a831d46567256b91f356f8
Jesse S <rhadamesjesse14rj@gmail.com>
2020-03-04 15:00:06 -0300

    Adding sound

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ let counter;
 let timerId;
 let session = 'session';
 
-let beep = new Audio('sounds/BeepSound.wav');
+let beep = new Audio('assets/js/sounds/BeepSound.wav');
 beep.volume = 0.2;
 
 if (session === 'session') {
