96d14272508c5c30635bcb01d30cc960f6b7c953
Jesse S <rhadamesjesse14rj@gmail.com>
2020-03-04 14:26:58 -0300

    first commit

---
+++ assets/js/Timer.js
@@ -0,0 +1,41 @@
+function convertSeconds(s) {
+    let min = Math.floor(s / 60);
+    let sec = s % 60;
+
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+
+    return min + ':' + sec;
+}
+
+function count() {
+    counter--;
+    timer.textContent = convertSeconds(counter);
+    console.log(counter);
+    if (counter < 1) {
+        session = (session === 'session') ? session = 'break' : session = 'session';
+        beep.play();
+        startButton.addEventListener('click', startButtonEvent); 
+        changeSession();
+        if (session === 'session') {
+            counter = sessionInput.value * 60;
+        } else {
+            counter =  breakInput.value * 60;
+        }
+        
+        timer.textContent = convertSeconds(counter);
+        stopCounter();
+    }
+}
+
+function startCounter() {
+    timerId = setInterval(count, 1000);
+}
+
+function stopCounter() {
+        clearInterval(timerId);
+}
---
+++ assets/js/index.js
@@ -0,0 +1,74 @@
+const timer = document.querySelector('#timer');
+const sessionTitle = document.querySelector('#session-title');
+const sessionInput = document.querySelector('#sessionInput');
+const breakInput = document.querySelector('#breakInput');
+const startButton = document.querySelector('#startTimer');
+let counter;
+let timerId;
+let session = 'session';
+
+let beep = new Audio('https://goo.gl/65cBl1');
+beep.volume = 0.2;
+
+console.log(beep.volume);
+
+
+
+if (session === 'session') {
+    counter = sessionInput.value * 60;
+} else {
+    counter =  breakInput.value * 60;
+}
+
+startButton.addEventListener('click', startButtonEvent);
+
+
+
+function startButtonEvent() {
+    changeSession();
+    switchPause();
+    startCounter();
+    startButton.removeEventListener('click', startButtonEvent, false);
+}
+
+sessionInput.addEventListener('click', function() {
+    if (session === 'session') {
+        counter = sessionInput.value * 60;
+        timer.textContent = convertSeconds(counter);
+    }
+});
+
+breakInput.addEventListener('click', function() {
+    if (!(session === 'session')) {
+        counter = breakInput.value * 60;
+        timer.textContent = convertSeconds(counter);
+    }
+});
+
+function changeSession() {
+    if (session === 'session') {
+        sessionTitle.textContent = 'Session';
+        startButton.textContent = 'start session';
+    } else {
+        sessionTitle.textContent = 'Break time';
+        startButton.textContent = 'start break';
+    }
+}
+
+function switchPause() {
+    if (session === 'session') {
+        startButton.textContent = 'pause session';
+        startButton.addEventListener('click', function() {
+            stopCounter();
+            startButton.textContent = 'continue ' + session;
+            startButton.addEventListener('click', startButtonEvent);
+        });
+    } else {
+        startButton.textContent = 'pause break';
+        startButton.addEventListener('click', function() {
+            stopCounter();
+            startButton.textContent = 'continue ' + session; 
+            startButton.addEventListener('click', startButtonEvent);
+        })
+    }
+}
